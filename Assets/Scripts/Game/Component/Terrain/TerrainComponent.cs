d2561d87a91ef8b43eba9b3a15d41933a747799b
Andrew <andrew.munro@dubitlimited.com>
2015-08-10 19:59:38 +0100

    Initial Commit

---
+++ Assets/Scripts/Game/Component/Terrain/TerrainComponent.cs
@@ -0,0 +1,12 @@
+﻿using Assets.Scripts.Framework.Component;
+
+namespace Assets.Scripts.Game.Component.Terrain
+{
+    public class TerrainComponent : AbstractGameComponent
+    {
+        public TerrainComponent()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.Framework;
+
+namespace Assets.Scripts.Game
+{
+    public class GameManager : AbstractGameManager 
+    {
+        public static new GameManager Instance { get; private set; }
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            Instance = this;
+        }
+    }
+}
